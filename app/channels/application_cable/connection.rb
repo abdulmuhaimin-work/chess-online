696cb62197bf2a06070f7fe4c4d0cb8d95e855ba
Abdul Muhaimin <abdulmuhaimin.work@gmail.com>
2016-09-30 12:43:44 +0800

    change matchmaking connection from using random uuid to using user id

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -3,7 +3,17 @@ module ApplicationCable
     identified_by :uuid
 
     def connect
-      self.uuid = SecureRandom.uuid
+      self.uuid = find_verified_user
+      logger.add_tags 'ActionCable', uuid.email
     end
+
+    protected
+      def find_verified_user
+        if verified_user = User.find_by(id: cookies.signed['user.id'])
+          verified_user
+        else
+          reject_unauthorized_connection
+        end
+      end
   end
 end
